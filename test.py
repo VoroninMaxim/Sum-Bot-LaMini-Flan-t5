67cfbb4f0e7dd455fd973f372e430aedbd8bb4bb
Maxim <maksimsazonov476@gmail.com>
2024-05-19 15:54:24 -0700

    add

---
+++ test.py
@@ -0,0 +1,9 @@
+
+from streamlit.testing.v1 import AppTest
+
+def test_project_main():
+    at = AppTest.from_file("projec_main.py").run()
+    assert not at.exception
+
+    # at.text_input('text').input('text').run()
+    # assert at.warning[0].value == 'Sorry, the text did not match'
